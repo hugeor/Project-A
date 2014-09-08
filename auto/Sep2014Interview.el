7d52b75048f8bfc655574a2f4fa7636c5c1c35cd
hugeor <hu.george@ymail.com>
2014-09-08 16:36:29 +0800

    first draft

---
+++ auto/Sep2014Interview.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "Sep2014Interview"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xeCJK"
+     "wrapfig"
+     "graphicx"
+     "utopia"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
