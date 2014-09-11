b0ef60d38b60428d5fb6f9a5842eaeb6971e8c8d
hugeor <hu.george@ymail.com>
2014-09-11 20:40:09 +0800

    new version for sep14

---
+++ auto/newSep14.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "newSep14"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xeCJK"
+     "wrapfig"
+     "graphicx"
+     "utopia"
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
