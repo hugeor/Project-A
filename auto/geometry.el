c5393b9c224507ce0866b54d69eb55f3f7d7ca40
hugeor <hu.george@ymail.com>
2014-11-04 00:56:11 +0800

    sec2 added

--- auto/geometry.el
+++ auto/geometry.el
@@ -1,10 +1,13 @@
 (TeX-add-style-hook "geometry"
  (lambda ()
+    (LaTeX-add-environments
+     "remark")
     (LaTeX-add-labels
      "s:topo"
      "s:mani"
      "s:vefi")
     (TeX-run-style-hooks
+     "amsthm"
      "fontenc"
      "T1"
      "jheppub"
